7ac0fee5dbbd17df0d8164736ab538bf4fa10f4f
abdulhafizk <hafizh.abdulhafizhkustiawan@gmail.com>
2024-04-15 14:32:58 +0700

    main

--- main.php
+++ main.php
@@ -27,13 +27,11 @@
             <!-- Content End -->
 
             <div class="fixed-buottom text-center mb-3">
-                Copyright 2024 Trio Squad For Portofolio Easometric and Programmers March
+                Copyright 2024
             </div>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+                crossorigin="anonymous"></script>
 </body>
 
 </html>
